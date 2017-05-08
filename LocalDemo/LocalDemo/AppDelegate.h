1faf2a66f22eaae2dd11fe677ac6c0e972e69eb1
who <who@163.com>
2017-05-08 13:56:42 +0800

    初始化项目

---
+++ LocalDemo/LocalDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalDemo
+//
+//  Created by HEJUN on 2017/5/8.
+//  Copyright © 2017年 tianxiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalDemo/LocalDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalDemo
+//
+//  Created by HEJUN on 2017/5/8.
+//  Copyright © 2017年 tianxiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
